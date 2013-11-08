a88f57e7a88859f310840a55d46de89213e22a9e
Leonardo Casasanta <leonardo2050@gmail.com>
2013-11-08 16:32:43 -0200

    Disponibilizando novos métodos para requisição de rota

--- src/br/com/lvc/googleplaces/facade/GoogleServicesFacade.java
+++ src/br/com/lvc/googleplaces/facade/GoogleServicesFacade.java
@@ -2,6 +2,8 @@ package br.com.lvc.googleplaces.facade;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.util.Log;
 import br.com.lvc.googleplaces.connection.HttpConnectionException;
@@ -13,19 +15,19 @@ import br.com.lvc.googleplaces.model.place.ResultPlaceRequest;
 import com.google.android.gms.maps.model.LatLng;
 
 public class GoogleServicesFacade extends WebServiceComun {
-	 
+
 	public static final String URL_PLACES_API = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"; // Lista locais pertinentes próximos a uma localização
 	public static final String URL_DIRECTIONS_API = "https://maps.googleapis.com/maps/api/directions/json?"; // Consegue traçar rotas entre varios pontos	 
 	public static final String URL_GEOCODE_API = "http://maps.googleapis.com/maps/api/geocode/json?"; // Permite saber uma localização com base em um endereço
-	
+
 	private static GoogleServicesFacade instance;
-	
+
 	public static GoogleServicesFacade getInstance() {
 		if(instance == null)
 			instance = new GoogleServicesFacade();
 		return instance;
 	}
-	
+
 	/**
 	 * 
 	 * @param address
@@ -39,45 +41,91 @@ public class GoogleServicesFacade extends WebServiceComun {
 		ResultCompleteGeocode result = sendDataGet(url, ResultCompleteGeocode.class);
 		return result;
 	}
-	 
+
 	public ResultPlaceRequest requestPlacesNearLocation(LatLng positionTarget, int radius, String key) throws HttpConnectionException {
 		String latLngString = latLngToString(positionTarget);
 		String url = URL_PLACES_API.concat("location=" + latLngString +"&radius="+ radius +"&sensor=true&key=" + key);
 		ResultPlaceRequest result = sendDataGet(url, ResultPlaceRequest.class);
 		return result;
 	}
-	 
-	
-	public ResultDirectionRequest requestWayToGoToDestiny(LatLng origin, String destination) throws HttpConnectionException {
-		//praca+sete+belo+horizonte,MG
-		String destinationPrepareted = prepareDestinationString(destination);
-		String url = generateURLDirection(origin, destinationPrepareted);
+
+
+	public ResultDirectionRequest requestWayToGoToDestiny(LatLng origin, String destination, List<LatLng> latLngs) throws HttpConnectionException {
+		List<String> wayPoints = toListString(latLngs);
+		String url = generateURLDirection(latLngToString(origin), prepareDestinationString(destination),wayPoints);
 		ResultDirectionRequest result = sendDataGet(url, ResultDirectionRequest.class);
-		
+
+		return result;
+	}
+
+	public ResultDirectionRequest requestWayToGoToDestiny(LatLng origin, LatLng destination, List<LatLng> latLngs) throws HttpConnectionException {
+		List<String> wayPoints = toListString(latLngs);
+		String url = generateURLDirection(latLngToString(origin), latLngToString(destination),wayPoints);
+		ResultDirectionRequest result = sendDataGet(url, ResultDirectionRequest.class);
+
+		return result;
+	}
+
+	public ResultDirectionRequest requestWayToGoToDestiny(String origin, String destination) throws HttpConnectionException { 
+		ResultDirectionRequest result = requestWayToGoToDestiny(origin,destination, null);
 		return result;
 	}
 	
+	public ResultDirectionRequest requestWayToGoToDestiny(LatLng origin, String destination ) throws HttpConnectionException {
+		ResultDirectionRequest result = requestWayToGoToDestiny(origin,destination, null);
+		return result;
+	}
+
+	public ResultDirectionRequest requestWayToGoToDestiny(LatLng origin, LatLng destination ) throws HttpConnectionException {
+		ResultDirectionRequest result = requestWayToGoToDestiny(origin,destination, null);
+		return result;
+	}
+
+	public ResultDirectionRequest requestWayToGoToDestiny(String origin, String destination, List<LatLng> latLngs) throws HttpConnectionException {
+		List<String> wayPoints = toListString(latLngs);
+		String url = generateURLDirection(prepareDestinationString(origin), prepareDestinationString(destination), wayPoints);
+		ResultDirectionRequest result = sendDataGet(url, ResultDirectionRequest.class);
+
+		return result;
+	}
+
+	private List<String> toListString(List<LatLng> latLngs) {
+		List<String> wayPoints = new ArrayList<String>();
+		if(latLngs == null)
+			return wayPoints;
+		
+		for(LatLng latLng : latLngs) {
+			String wayPoint = latLngToString(latLng);
+			wayPoints.add(wayPoint);
+		}
+		return wayPoints;
+	}
+
 	private String prepareDestinationString(String destination) {
 		String destionationResult = destination.trim().replace(" ", "+");
 		return destionationResult;
 	}
-	
-	
-	private String generateURLDirection(LatLng postionOrigin, String destination) {
+
+
+	private String generateURLDirection(String positionOrigin, String destination, List<String> wayPoints) {
 		StringBuilder stringBuilder = new StringBuilder();
-		
-		stringBuilder.append("origin=" + latLngToString(postionOrigin));
+
+		stringBuilder.append("origin=" + positionOrigin);
 
 		stringBuilder.append("&");
 		stringBuilder.append("destination=" + destination);
 
 		stringBuilder.append("&");
-		stringBuilder.append("sensor=false");
-	 
-	/*	stringBuilder.append("&");
-		String pipe = encodeUTF8("|");
-		stringBuilder.append("waypoints=optimize:true"+pipe+"Concórdia+Belo+Horizonte,MG"+pipe+"Aparecida+Belo+Horizonte,MG"); 
-	*/ 
+		stringBuilder.append("sensor=true");
+
+		if(wayPoints != null && !wayPoints.isEmpty()) {
+			stringBuilder.append("&");
+			String pipe = encodeUTF8("|");
+			stringBuilder.append("waypoints=optimize:true");
+			for(String wayPoint : wayPoints) {
+				stringBuilder.append(pipe + wayPoint);	
+			}
+		}
 
 		stringBuilder.append("&");
 		stringBuilder.append("unit=metric");
@@ -87,16 +135,16 @@ public class GoogleServicesFacade extends WebServiceComun {
 		Log.i("URL FINAL", "url: " + novaURL);
 		return novaURL;
 	}
-	
+
 	private String latLngToString(LatLng latLng) {
 		String lat = String.valueOf(latLng.latitude);
 		String lng = String.valueOf(latLng.longitude);
-		
+
 		return lat.concat(",").concat(lng);
 	}
-	
+
 	private String encodeUTF8(String url) {
-		
+
 		String result = null;
 
 		try { 
